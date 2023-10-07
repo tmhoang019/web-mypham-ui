f8495e06aa0510fe17cd22d224a13db87b96ce4c
tmhoang019 <thachminhhoangk19@gmail.com>
2023-10-07 12:31:23 +0700

    Install sass, GlobalStyles, Router web layout 1

---
+++ src/pages/DetailProduct/index.js
@@ -0,0 +1,5 @@
+function DetailProduct() {
+    return <h2>DetailProduct Page</h2>;
+}
+
+export default DetailProduct;
---
+++ src/pages/ListProduct/index.js
@@ -0,0 +1,5 @@
+function ListProduct() {
+    return <h2>ListProduct Page</h2>;
+}
+
+export default ListProduct;
